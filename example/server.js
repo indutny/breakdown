eaf48fa22863f12ceeb43faed413a671e3a6d773
Fedor Indutny <fedor@indutny.com>
2020-07-15 14:44:06 -0700

    initial

---
+++ example/server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const http = require('http');
+
+const Breakdown = require('./');
+
+const b = new Breakdown();
+
+b.start('log.json');
+
+process.on('SIGINT', () => {
+  b.stop();
+  process.exit();
+});
+
+const agent = new http.Agent({
+  keepAlive: true,
+  maxSockets: 1,
+  maxFreeSockets: 1,
+  timeout: 60 * 1000,
+});
+
+const get = (path, callback) => {
+  http.request({
+    agent,
+    host: 'example.com',
+
+    method: 'GET',
+    path,
+  }, callback).end();
+};
+
+http.createServer((req, res) => {
+  get('/', (remote) => {
+    remote.resume();
+    remote.on('end', () => {
+      get('/', (remote) => {
+        remote.pipe(res);
+      });
+    });
+  });
+}).listen(8000);
